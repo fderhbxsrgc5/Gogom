a8422c4e0f840e8f6d8cc51af5cc0b7dbda679d7
vernxpork <shotigel1@gmail.com>
2026-01-15 16:34:06 +0800

    Create js/app.js

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
+});
+if (localStorage.getItem('darkMode') === 'true') document.body.classList.add('dark-mode');
